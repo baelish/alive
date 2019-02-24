d4eb46f714390b7f8303edf4806ab7c125886c7d
Harold Drost <baelish@bluecell.net>
2019-02-24 16:01:32 +0000

    Use json file

--- alive.go
+++ alive.go
@@ -1,60 +1,41 @@
 package main
 
 import (
+    "encoding/json"
 	"fmt"
+    "io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Box struct {
+    Name string `json:"name"`
+    Size string `json:"size"`
+    Color string `json:"color"`
+}
+
+type Boxes struct {
+    Boxes []Box `json:"boxes"`
+}
+
+func loadJsonFromFile(jsonFile string) Boxes {
+    byteValue, err := ioutil.ReadFile(jsonFile)
+    if err != nil {
+        log.Fatal(err)
+    }
+    var boxes Boxes
+    json.Unmarshal(byteValue, &boxes)
+    return boxes
+}
+
+
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<head><link rel='stylesheet' type='text/css' href='css/standard.css'/></head>")
+    boxes := loadJsonFromFile("/home/drosth/go/src/github.com/baelish/alive/test.json")
+	fmt.Fprintf(w, "<head><meta http-equiv='refresh' content='5'><link rel='stylesheet' type='text/css' href='css/standard.css'/></head>")
 	fmt.Fprintf(w, "<div class='big-box'>")
-	fmt.Fprintf(w, "<div class='green xlarge box'>Extra Large</div>")
-	fmt.Fprintf(w, "<div class='green dlarge box'>Large</div>")
-	fmt.Fprintf(w, "<div class='green large box'>Double Large</div>")
-	fmt.Fprintf(w, "<div class='green dmedium box'>Double Medium</div>")
-	fmt.Fprintf(w, "<div class='green medium box'>Medium</div>")
-	fmt.Fprintf(w, "<div class='grey medium box'>Medium</div>")
-	fmt.Fprintf(w, "<div class='green medium box'>Medium</div>")
-	fmt.Fprintf(w, "<div class='green medium box'>Medium</div>")
-	fmt.Fprintf(w, "<div class='green dsmall box'>Double Small</div>")
-	fmt.Fprintf(w, "<div class='green dsmall box'>Double Small</div>")
-	fmt.Fprintf(w, "<div class='grey dsmall box'>Double Small</div>")
-	fmt.Fprintf(w, "<div class='grey small box'>Small</div>")
-	fmt.Fprintf(w, "<div class='green small box'>Small</div>")
-	fmt.Fprintf(w, "<div class='green small box'>Small</div>")
-	fmt.Fprintf(w, "<div class='amber small box'>Small</div>")
-	fmt.Fprintf(w, "<div class='green small box'>Small</div>")
-	fmt.Fprintf(w, "<div class='green small box'>Small</div>")
-	fmt.Fprintf(w, "<div class='green dmicro box'>D Micro</div>")
-	fmt.Fprintf(w, "<div class='green dmicro box'>D Micro</div>")
-	fmt.Fprintf(w, "<div class='red dmicro box'>D Micro</div>")
-	fmt.Fprintf(w, "<div class='green dmicro box'>D Micro</div>")
-	fmt.Fprintf(w, "<div class='green dmicro box'>D Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='red micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
-	fmt.Fprintf(w, "<div class='green micro box'>Micro</div>")
+    for i := 0; i < len(boxes.Boxes); i++ {
+	    fmt.Fprintf(w, "<div class='%s %s box'>%s</div>", boxes.Boxes[i].Color, boxes.Boxes[i].Size, boxes.Boxes[i].Name)
+    }
 	fmt.Fprintf(w, "</div>")
 }
 
