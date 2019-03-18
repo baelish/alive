d95512d6d1d8fe76b118d0e4fa9cc81bef2dcca7
Harold Drost <baelish@bluecell.net>
2019-03-18 21:14:55 +0000

    Adding events, untested.

--- api-v1.go
+++ api-v1.go
@@ -7,6 +7,13 @@ import (
 	"net/http"
 )
 
+type Event struct {
+	Id          string `json:"id"`
+	Color       string `json:"color"`
+	Message 		string `json:lastMessage`
+}
+
+
 func apiGetBoxes(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(boxes)
 }
@@ -21,12 +28,19 @@ func apiGetBox(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Box{})
 }
 
-func apiCreateBox(w http.ResponseWriter, r *http.Request) {}
+func apiCreateEvent(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var event Event
+	_ = json.NewDecoder(r.Body).Decode(&event)
+	event.Id = params["id"]
+	json.NewEncoder(w).Encode(event)
+}
 func apiDeleteBox(w http.ResponseWriter, r *http.Request) {}
 
 func runApi() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/", apiGetBoxes).Methods("GET")
 	router.HandleFunc("/api/v1/{id}", apiGetBox).Methods("GET")
+	router.HandleFunc("/api/v1/events/{id}", apiCreateEvent).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
