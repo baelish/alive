7fcd343acb6ce9b0e19945ca963149de40ac1e79
Harold Drost <baelish@bluecell.net>
2025-05-30 20:22:59 +0100

    Use full width of page for infoboxes
    Also used templates in a better way.

--- static-source/scripts.js
+++ static-source/scripts.js
@@ -1,37 +1,45 @@
 /* jshint esversion: 6 */
 
 // Reload if re-visiting using back/forward buttons.
-if (String(window.performance.getEntriesByType("navigation")[0].type) === "back_forward"){
+if (
+  String(window.performance.getEntriesByType("navigation")[0].type) ===
+  "back_forward"
+) {
   location.reload();
 }
 
-
 // Register with box event source
 let source = new EventSource("/events/");
-source.onmessage = function(event) {
+source.onmessage = function (event) {
   event = JSON.parse(event.data);
-  switch(event.type) {
+  switch (event.type) {
     case "keepalive":
       keepalive();
 
       break;
 
     case "updateBox":
-      if ( window.location.pathname === "/" || window.location.pathname === `/box/${event.id}`) {
+      if (
+        window.location.pathname === "/" ||
+        window.location.pathname === `/box/${event.id}`
+      ) {
         updateBox(event);
       }
 
       break;
 
     case "deleteBox":
-      if ( window.location.pathname === "/" || window.location.pathname === `/box/${event.id}`) {
+      if (
+        window.location.pathname === "/" ||
+        window.location.pathname === `/box/${event.id}`
+      ) {
         deleteBox(event.id);
       }
 
       break;
 
     case "createBox":
-      if ( window.location.pathname === "/" ) {
+      if (window.location.pathname === "/") {
         createBox(event.after, event.box);
       }
 
@@ -42,7 +50,6 @@ source.onmessage = function(event) {
   }
 };
 
-
 // Box tooltip
 function boxHover(tip) {
   let target = document.getElementById("tooltip");
@@ -50,19 +57,16 @@ function boxHover(tip) {
   target.display = "block";
 }
 
-
 function boxOut() {
   let target = document.getElementById("tooltip");
   target.innerHTML = "";
   target.display = "hidden";
 }
 
-
 // Box click
 function boxClick(id) {
   window.location.href = "/box/" + id;
 }
-
 
 // Create box
 function createBox(after, box) {
@@ -87,13 +91,11 @@ function createBox(after, box) {
   precedingBox.insertAdjacentHTML("afterEnd", divContent);
 }
 
-
 // Remove box
 function deleteBox(id) {
   let target = document.getElementById(id);
   target.parentNode.removeChild(target);
 }
-
 
 // Update box
 function updateBox(event) {
@@ -105,46 +107,55 @@ function updateBox(event) {
 
   if (event.maxTBU) {
     let t = targetBox.getElementsByClassName("maxTBU")[0];
-    if (t.tagName === 'TR') {
-      t.getElementsByTagName('TD')[0].innerHTML = event.maxTBU;
-      if (event.maxTBU === "0s") {t.style.display = "none";} else {t.style.display = "table-row";}
+    if (t.tagName === "TR") {
+      t.getElementsByTagName("TD")[0].innerHTML = event.maxTBU;
+      if (event.maxTBU === "0s") {
+        t.style.display = "none";
+      } else {
+        t.style.display = "table-row";
+      }
     } else {
-      t.innerHTML = event.maxTBU
+      t.innerHTML = event.maxTBU;
     }
   }
 
   if (event.expireAfter) {
     let t = targetBox.getElementsByClassName("expireAfter")[0];
-    if (t.tagName === 'TR') {
-      t.getElementsByTagName('TD')[0].innerHTML = event.expireAfter;
-      if (event.expireAfter === "0s") {t.style.display = "none";} else {t.style.display = "table-row";}
+    if (t.tagName === "TR") {
+      t.getElementsByTagName("TD")[0].innerHTML = event.expireAfter;
+      if (event.expireAfter === "0s") {
+        t.style.display = "none";
+      } else {
+        t.style.display = "table-row";
+      }
     } else {
-      t.innerHTML = event.expireAfter
+      t.innerHTML = event.expireAfter;
     }
   }
 }
-
 
 // keepalive
 let lastKa;
 function keepalive() {
   let ct = new Date().getTime();
-  if ( lastKa && (lastKa + 60000) < ct ) { location.reload();}
+  if (lastKa && lastKa + 60000 < ct) {
+    location.reload();
+  }
   lastKa = ct;
   let target = document.getElementById("status-bar");
-  target.classList.remove("amber","green","grey","noUpdate","red");
+  target.classList.remove("amber", "green", "grey", "noUpdate", "red");
   target.classList.add("green");
   target.getElementsByClassName("message")[0].innerHTML = "";
-  if(typeof ka !== "undefined") { clearTimeout(ka); }
-  ka = setTimeout(
-    function(){
-      target.classList.remove("amber","green","grey","noUpdate","red");
-      target.classList.add("noUpdate");
-      target.getElementsByClassName("message")[0].innerHTML = "ERROR: No keepalives since " + myTime(lastKa) + ".";
-    }, 5 * 1000
-  );
+  if (typeof ka !== "undefined") {
+    clearTimeout(ka);
+  }
+  ka = setTimeout(function () {
+    target.classList.remove("amber", "green", "grey", "noUpdate", "red");
+    target.classList.add("noUpdate");
+    target.getElementsByClassName("message")[0].innerHTML =
+      "ERROR: No keepalives since " + myTime(lastKa) + ".";
+  }, 5 * 1000);
 }
-
 
 // Print time in my preferred format
 function myTime(t) {
@@ -157,36 +168,49 @@ function myTime(t) {
   return r.toISOString();
 }
 
-
 // Pad a string
 function pad(n, width, z) {
-  z = z || '0';
-  n = n + '';
+  z = z || "0";
+  n = n + "";
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-
 // Change alert level of a box
 function changeAlertLevel(target, status, message) {
-  if ( ["amber","green","grey","noUpdate","red"].indexOf(status) === -1) { status = "grey";}
+  if (["amber", "green", "grey", "noUpdate", "red"].indexOf(status) === -1) {
+    status = "grey";
+  }
   target.classList.remove("amber", "green", "grey", "noUpdate", "red");
   target.classList.add(status);
   target.getElementsByClassName("message")[0].innerHTML = message;
   target.getElementsByClassName("lastUpdated")[0].innerHTML = myTime();
   let pMessages = target.getElementsByClassName("previousMessages");
-  if (typeof(pMessages[0] != 'undefined') && pMessages[0] != null) {
-    pMessages[0].insertAdjacentHTML('afterbegin', "<li>" + myTime() + ": " + status.toUpperCase() + " (" + message + ")</li>");
+  if (typeof (pMessages[0] != "undefined") && pMessages[0] != null) {
+    pMessages[0].insertAdjacentHTML(
+      "afterbegin",
+      "<li>" +
+        myTime() +
+        ": " +
+        status.toUpperCase() +
+        " (" +
+        message +
+        ")</li>",
+    );
   }
 }
 
-
 // Make big box to fit as many biggest boxes as will fit the current window.
-function rightSizeBigBox() {
-  let availableWidth = Math.floor((window.innerWidth -30) / 512) * 512;
-  let widthBox = (availableWidth >= 512) ? availableWidth:512;
-  document.getElementById('big-box').style.width = widthBox + "px";
-  let fullWidthBoxes = document.getElementsByClassName('fullwidth');
+function rightSizeBigBox(pageType) {
+  let widthBox;
+  if (pageType === "dashboard") {
+    let availableWidth = Math.floor((window.innerWidth - 30) / 512) * 512;
+    widthBox = availableWidth >= 512 ? availableWidth : 512;
+  } else {
+    widthBox = window.innerWidth - 30;
+  }
+  document.getElementById("big-box").style.width = widthBox + "px";
+  let fullWidthBoxes = document.getElementsByClassName("fullwidth");
   for (let i = 0; i < fullWidthBoxes.length; i++) {
-    fullWidthBoxes[i].style.width = (widthBox - 2) + "px";
+    fullWidthBoxes[i].style.width = widthBox - 2 + "px";
   }
 }
