f0abc6fe1a573d0cde1ef908a90855b8fefa3248
Harold Drost <baelish@bluecell.net>
2025-10-31 16:45:35 +0000

    Stop using go-bindata in favor of go embed.

--- internal/server/create-static-content.go
+++ internal/server/create-static-content.go
@@ -12,16 +12,16 @@ func createStaticContent() {
 	if options.Debug {
 		logger.Info("Creating Static Content")
 	}
-	for _, file := range AssetNames() {
+	for _, file := range embeddedAssetNames() {
 		if _, err := os.Stat(options.StaticPath + "/" + file); os.IsNotExist(err) {
 			logger.Info("file doesn't exist, creating default file", zap.String("file", options.StaticPath+file))
-			err = RestoreAsset(options.StaticPath, file)
+			err = restoreEmbeddedAsset(options.StaticPath, file)
 			if err != nil {
 				logger.Error(err.Error())
 			}
 		} else if options.DefaultStatic {
 			logger.Info("default files enforced, creating default file", zap.String("file", options.StaticPath+file))
-			err = RestoreAsset(options.StaticPath, file)
+			err = restoreEmbeddedAsset(options.StaticPath, file)
 			if err != nil {
 				logger.Error(err.Error())
 			}
